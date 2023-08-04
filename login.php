e0d14b20826b0846e1676be853c701405ff06d0a
dasuntheekshanagit <dasun.theekshana.git@gmail.com>
2023-08-04 17:43:17 +0530

    create login and logout function

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 // Check if the form was submitted
 if (isset($_POST['submit'])) {
     // Retrieve the input values
@@ -10,8 +12,9 @@ if (isset($_POST['submit'])) {
     $defaultPassword = '';
 
     if ($username === $defaultUsername && $password === $defaultPassword) {
+        $_SESSION['user_id'] = $username;
         // Login successful, redirect to welcome.php
-        header("Location: welcome.php");
+        header("Location: home.html");
         exit();
     } else {
         // Invalid username or password, display an error message or redirect back to the login page
