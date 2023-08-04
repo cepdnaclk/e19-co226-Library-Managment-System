4626a0f091db346b6196e88e7c51219d31c1a2a5
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-08-04 20:47:59 +0530

    Update create_tables.php and return_book.php

--- return_book.php
+++ return_book.php
@@ -13,6 +13,29 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Check if the returned_books table exists
+$tableExistsQuery = "SHOW TABLES LIKE 'returned_books'";
+$tableExistsResult = $conn->query($tableExistsQuery);
+
+if ($tableExistsResult->num_rows === 0) {
+    // Table does not exist, create it
+    $createTableQuery = "CREATE TABLE returned_books (
+        LoanID INT NOT NULL,
+        BookID INT NOT NULL,
+        BorrowerID INT NOT NULL,
+        PRIMARY KEY (LoanID),
+        FOREIGN KEY (LoanID) REFERENCES loantransaction(LoanID),
+        FOREIGN KEY (BookID) REFERENCES book(BookID),
+        FOREIGN KEY (BorrowerID) REFERENCES borrower(BorrowerID)
+    )";
+    
+    if ($conn->query($createTableQuery)) {
+        echo "Table 'returned_books' created successfully.";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+}
+
 // Check if a book is being returned
 if (isset($_POST['return'])) {
     $loanID = $_POST['loan_id'];
