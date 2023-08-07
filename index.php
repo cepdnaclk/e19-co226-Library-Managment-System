b4257393dd85f8ccd569cf1ad8aee9a45dd78aa3
dasuntheekshanagit <dasun.theekshana.git@gmail.com>
2023-08-07 10:21:10 +0530

    redirect acording to the role

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.html");
+if (isset($_SESSION['user_id'])) {
+    if ($_SESSION['roleid'] == 'staff') {
+        header("Location: libraryhome.html");
+    } else {
+        header("Location: login.html");
+    }
     exit();
 }
 
