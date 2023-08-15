9cd1f1636a77e9447a8a356ad89c762d5bd23de3
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-08-15 20:24:22 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Database connection credentials
 $host = 'sql.freedb.tech';
-$dbUsername = 'freedb_co226p';
-$dbPassword = 'aSUyQe6z573F$7T';
-$dbName = 'freedb_co_226';
+$dbUsername = 'freedb_freedb_co226p';
+$dbPassword = '&QBrR&4rQtMg8Aw';
+$dbName = 'freedb_freedb_co_226';
 ?>
