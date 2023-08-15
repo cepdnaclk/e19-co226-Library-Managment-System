74b46ee5d1400a301dde6d469b33592c3d4cecad
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-08-16 02:02:12 +0530

    Update returnbook.php

--- returnbook.php
+++ returnbook.php
@@ -7,19 +7,19 @@
     <h1>Return Books</h1>
     <hr>
     <form action="return_book.php" method="post" class="return">
-        <div class="inputBox">
+        <div class="loan">
             <label for="user_id">User ID:</label>
             <input type="text" name="user_id" id="user_id" placeholder="Enter User ID" required>
         </div>
-        <div class="inputBox">
+        <div class="loan">
             <label for="book_id">Book ID:</label>
             <input type="text" name="book_id" id="book_id" placeholder="Enter Book ID" required>
         </div>
-        <div class="inputBox">
+        <div class="loan">
             <label for="return_date">Return Date:</label>
             <input type="date" name="return_date" id="return_date" required>
         </div>
-        <div class="inputBox">
+        <div class="loan-btn">
             <input type="submit" name="return" value="Return">
         </div>
     </form>
