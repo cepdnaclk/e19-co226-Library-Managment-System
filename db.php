179ab1673f4642142423a5e178a8ada726d990aa
dasuntheekshanagit <dasun.theekshana.git@gmail.com>
2023-08-16 13:50:28 +0530

    update pssword

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Database connection credentials
 $host = 'sql.freedb.tech';
-$dbUsername = 'freedb_freedb_CO226_lms';
-$dbPassword = 'ZzS%wqu@D8?w%wD';
+$dbUsername = 'freedb_CO226_lms';
+$dbPassword = 'NAWjsk%69WkpX*9';
 $dbName = 'freedb_CO226_lms';
 ?>
